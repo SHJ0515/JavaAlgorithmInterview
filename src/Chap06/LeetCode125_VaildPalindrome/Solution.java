aab7145ba83071067176ed459e30d93cb02d1c46
송현준 <70439776+SHJ0515@users.noreply.github.com>
2024-08-25 22:37:42 +0900

    feat : Valid Palindrome
    6장 leetcode125 문제 Valid Palindrome

---
+++ src/Chap06/LeetCode125_VaildPalindrome/Solution.java
@@ -0,0 +1,19 @@
+package Chap06.LeetCode125_VaildPalindrome;
+
+public class Solution {
+    public boolean isPalindrome(String s) {
+
+
+        return false;
+    }
+}
+
+/*
+팰린드롬 : 앞뒤가 똑같은 단어나 문장으로, 뒤집어도 같은 말이 되는 단어 or 문장.
+
+A phrase is a palindrome if, after converting all uppercase letters
+into lowercase letters and removing all non-alphanumeric characters,
+it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+ */
