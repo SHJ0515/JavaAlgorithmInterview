6aaf99e6d4030a39d89935d1d1cb8a36b7ba1412
송현준 <70439776+SHJ0515@users.noreply.github.com>
2024-08-28 00:34:18 +0900

    Valid Palindrome solve1
    leetcode 125번 Valid Palindrome 문제
    
    처음 생각한 풀이.
    - 받은 문자열을 영문자, 숫자 제외하고는 모두 없애고 새로운 문자열을 만듬, 이 때 대문자는 소문자로 바꿔서 만듬
    - 새로운 문자열을 맨 앞과 맨 뒤를 비교해가면서 전부 일치 시 true 아닌 경우 발생 시 false

--- src/Chap06/LeetCode125_VaildPalindrome/Solution.java
+++ src/Chap06/LeetCode125_VaildPalindrome/Solution.java
@@ -2,14 +2,52 @@ package Chap06.LeetCode125_VaildPalindrome;
 
 public class Solution {
     public boolean isPalindrome(String s) {
+        String change = change(s);
+        int length = change.length();
 
+        int start = 0;
+        int end = length - 1;
 
-        return false;
+        while (start < end) {
+            char c1 = change.charAt(start);
+            char c2 = change.charAt(end);
+
+            if (c1 != c2) {
+                return false;
+            } else {
+                start++;
+                end--;
+            }
+
+        }
+        return true;
+    }
+    
+    public String change(String s) {
+        StringBuilder newString = new StringBuilder();
+        int count = s.length();
+
+        for (int i = 0; i < count; i++) {
+            char current = s.charAt(i);
+            if (current >= 'a' && current <= 'z' || current >= '0' && current <= '9') {
+                newString.append(current);
+            } else if (current >= 'A' && current <= 'Z') {
+                current = Character.toLowerCase(current);
+                newString.append(current);
+            }
+        }
+        return newString.toString();
     }
 }
 
 /*
 팰린드롬 : 앞뒤가 똑같은 단어나 문장으로, 뒤집어도 같은 말이 되는 단어 or 문장.
+영문자와 숫자만을 대상으로 하며, 대소문자 구분 하지 않음.
+
+처음 생각한 풀이.
+- 받은 문자열을 영문자, 숫자 제외하고는 모두 없애고 새로운 문자열을 만듬, 이 때 대문자는 소문자로 바꿔서 만듬
+- 새로운 문자열을 맨 앞과 맨 뒤를 비교해가면서 전부 일치 시 true 아닌 경우 발생 시 false
+
 
 A phrase is a palindrome if, after converting all uppercase letters
 into lowercase letters and removing all non-alphanumeric characters,
--- src/Chap06/LeetCode125_VaildPalindrome/SolutionTest.java
+++ src/Chap06/LeetCode125_VaildPalindrome/SolutionTest.java
@@ -3,7 +3,7 @@ package Chap06.LeetCode125_VaildPalindrome;
 public class SolutionTest {
     public static void main(String[] args) {
         Solution s = new Solution();
-        String test1 = "Race a car";
+        String test1 = "Do geese see God?";
         String test2 = "Just Failed Result";
 
         System.out.println(s.isPalindrome(test1));
