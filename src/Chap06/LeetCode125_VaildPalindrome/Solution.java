e7efb1ffa45bd198ac617ee40b31bdd5daebb7cb
송현준 <70439776+SHJ0515@users.noreply.github.com>
2024-08-28 01:31:57 +0900

    refactor : Valid Palindrome solve1
    코드 수정
    1. 메소드 명 변경 : change 메소드를 filterString 으로 변경, 메소드 기능 더 명확하게 설명하기
    2. 문자열 필터링 개선 : toCharArray 사용해서 반복문을 더 간단하게 만듬
    3. isLetterOrDigit() 문자또는 숫자인지 판별 / toLowerCase() 영어대문자를 소문자로 변환 을 사용

--- src/Chap06/LeetCode125_VaildPalindrome/Solution.java
+++ src/Chap06/LeetCode125_VaildPalindrome/Solution.java
@@ -2,6 +2,46 @@ package Chap06.LeetCode125_VaildPalindrome;
 
 public class Solution {
     public boolean isPalindrome(String s) {
+        String filteredString = filterString(s);
+        int length = filteredString.length();
+        int start = 0;
+        int end = length - 1;
+
+        while(start < end){
+            if (filteredString.charAt(start) != filteredString.charAt(end)) {
+                return false;
+            }
+            else{
+                start++;
+                end--;
+            }
+        }
+
+        return true;
+    }
+
+    private String filterString(String s) {
+        StringBuilder newString = new StringBuilder();
+        for (char current : s.toCharArray()) {
+            if (Character.isLetterOrDigit(current)) {
+                newString.append(Character.toLowerCase(current));
+            }
+        }
+        return newString.toString();
+    }
+}
+/*
+코드 수정
+1. 메소드 명 변경 : change 메소드를 filterString 으로 변경, 메소드 기능 더 명확하게 설명하기
+2. 문자열 필터링 개선 : toCharArray 사용해서 반복문을 더 간단하게 만듬
+3. isLetterOrDigit() 문자또는 숫자인지 판별 / toLowerCase() 영어대문자를 소문자로 변환 을 사용
+4.
+ */
+
+
+/*
+public class Solution {
+    public boolean isPalindrome(String s) {
         String change = change(s);
         int length = change.length();
 
@@ -22,7 +62,7 @@ public class Solution {
         }
         return true;
     }
-    
+
     public String change(String s) {
         StringBuilder newString = new StringBuilder();
         int count = s.length();
@@ -39,6 +79,8 @@ public class Solution {
         return newString.toString();
     }
 }
+*/
+
 
 /*
 팰린드롬 : 앞뒤가 똑같은 단어나 문장으로, 뒤집어도 같은 말이 되는 단어 or 문장.
--- src/Chap06/LeetCode125_VaildPalindrome/SolutionTest.java
+++ src/Chap06/LeetCode125_VaildPalindrome/SolutionTest.java
@@ -3,8 +3,10 @@ package Chap06.LeetCode125_VaildPalindrome;
 public class SolutionTest {
     public static void main(String[] args) {
         Solution s = new Solution();
+        Solution2 solution2 = new Solution2();
+
         String test1 = "Do geese see God?";
-        String test2 = "Just Failed Result";
+        String test2 = "Just Failed Result 123!@#";
 
         System.out.println(s.isPalindrome(test1));
         System.out.println(s.isPalindrome(test2));
