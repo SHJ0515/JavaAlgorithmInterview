5036919340bfbf4c7bbb371bc1ca8b0ba351b723
송현준 <70439776+SHJ0515@users.noreply.github.com>
2024-08-29 00:39:42 +0900

    String, StringBuilder 메서드들
    String, StringBuilder 자료형에서 유용한 메서드 정리

--- src/Chap06/LeetCode125_VaildPalindrome/SolutionTest.java
+++ src/Chap06/LeetCode125_VaildPalindrome/SolutionTest.java
@@ -15,3 +15,23 @@ public class SolutionTest {
         System.out.println(solution2.isPalindrome(test2));
     }
 }
+
+/*
+String 자료형 유용한 메서드
+- 길이확인 : length()
+- 문자추출 : charAt(int index)
+- 부분 문자열 추출 : subString(int begin, int end)
+- 문자열 찾기 : indexOf(String str)
+- 문자열 대체 : replace(String target, String replace)
+- 대소문자 전환 : toUpperCase(), toLowerCase()
+- 문자열 분할 : split(String str)
+
+StringBuilder 자료형 유용한 메서드
+- 문자 추가 : append(String str)
+- 문자 삭제 : delete(int start, int end)
+- 문자 삽입 : insert(int offset, String str)
+- 문자열 길이 확인 : length
+- 문자열 뒤집기 : reverse()
+- 문자열 전환 : toString()
+ */
+
