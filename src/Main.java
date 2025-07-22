ff48b1645721c87bf048b0de5b54ca92a8651650
송현준 <70439776+SHJ0515@users.noreply.github.com>
2025-07-22 14:26:14 +0900

    code template

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
+
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        String[] tokens;
+        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine()); // 한 줄 입력 후 정수 변환
+
+
+
+
 
         br.close(); // BufferedReader 닫기
-        bw.flush();
-        bw.close();
     }
 }
