3325686afc04d93b44f7f2ebfc6dc4060e74b863
송현준 <70439776+SHJ0515@users.noreply.github.com>
2026-08-19 09:26:43 +0900

    boj11728 배열 합치기 - mergeSort 주석 추가

--- problems/백준/Silver V/11728_ 배열 합치기/boj11728.java
+++ problems/백준/Silver V/11728_ 배열 합치기/boj11728.java
@@ -65,3 +65,77 @@ public class boj11728 {
         System.out.println(sb);
     }
 }
+
+/*
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        int n  = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        mergeSort(0, n);
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    // [start, end)
+    public static void mergeSort(int start, int end) {
+        int mid = (start + end) / 2;
+
+        if (end - start <= 1) {
+            return;
+        }
+
+        mergeSort(start, mid);
+        mergeSort(mid, end);
+        merge(start,end);
+    }
+
+    public static void merge(int start, int end) {
+        int[] tmpArr = new int[end - start];
+        int tmp = 0;
+        int mid = (start + end) / 2;
+
+        int p1 = start;
+        int p2 = mid;
+
+        while (p1 < mid && p2 < end) {
+            if (arr[p1] <= arr[p2]) {
+                tmpArr[tmp++] = arr[p1];
+                p1++;
+            }
+            else {
+                tmpArr[tmp++] = arr[p2];
+                p2++;
+            }
+        }
+
+        if (p1 == mid) {
+            while (p2 < end){
+                tmpArr[tmp++] = arr[p2];
+                p2++;
+            }
+        }
+        else if (p2 == end){
+            while (p1 < mid){
+                tmpArr[tmp++] = arr[p1];
+                p1++;
+            }
+        }
+
+        for (int i = 0; i < tmpArr.length; i++) {
+            arr[start + i] = tmpArr[i];
+        }
+    }
+}
+ */
